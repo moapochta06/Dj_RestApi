d920cdda65f01de09ee18b02882076056d888190
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2024-11-13 17:43:48 +0700

    added return data in HTML format

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,23 @@
-from rest_framework import generics
-from .models import Snippet
-from rest_framework import permissions
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import generics, renderers
+from .models import Snippet  
+from .serializers import SnippetSerializer 
+from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+   return Response({
+       'users': reverse('user-list', request=request, format=format),
+       'snippets': reverse('snippet-list', request=request, format=format)
+   })
+
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
@@ -28,4 +41,12 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
    queryset = User.objects.all()
    serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+   queryset = Snippet.objects.all()
+   renderer_classes = [renderers.StaticHTMLRenderer]
+
+   def get(self, request, **kwargs):
+       snippet = self.get_object()
+       return Response(snippet.highlighted)
 
