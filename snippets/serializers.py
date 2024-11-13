c4230164e59ec009aa5119cfcc92dc453e34e83c
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2024-11-13 16:38:36 +0700

    added view for users

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Snippet
 
 
@@ -6,5 +7,12 @@ class SnippetSerializer(serializers.ModelSerializer):
    class Meta:
        model = Snippet
        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+class UserSerializer(serializers.ModelSerializer):
+   snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+   class Meta:
+       model = User
+       fields = ['id', 'username', 'snippets']
 
 
--- snippets/views.py
+++ snippets/views.py
@@ -1,56 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from .models import Snippet
-from .serializers import SnippetSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Snippet
-from .serializers import SnippetSerializer
-
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from .models import Snippet
-from .serializers import SnippetSerializer
-
-
-from rest_framework import mixins
 from rest_framework import generics
-
 from .models import Snippet
-from .serializers import SnippetSerializer
+
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import SnippetSerializer, UserSerializer
 
 
-class SnippetList(mixins.ListModelMixin,
-                 mixins.CreateModelMixin,
-                 generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
 
-   def get(self, request, *args, **kwargs):
-       return self.list(request, *args, **kwargs)
 
-   def post(self, request, *args, **kwargs):
-       return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
 
-   def get(self, request, *args, **kwargs):
-       return self.retrieve(request, *args, **kwargs)
+class UserList(generics.ListAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
 
-   def put(self, request, *args, **kwargs):
-       return self.update(request, *args, **kwargs)
 
-   def delete(self, request, *args, **kwargs):
-       return self.destroy(request, *args, **kwargs)
+class UserDetail(generics.RetrieveAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
